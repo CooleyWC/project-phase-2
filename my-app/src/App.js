d7bb160ec7f65ea0509fe77248ad7975ff95c9bb
Will Cooley <william.cooley@gmail.coom>
2023-10-26 15:48:13 -0400

    added useEffect and useState to App.js to fetch data in db.json

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,23 @@
-
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [questionData, setQuestionData] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(()=>{
+    fetch('http://localhost:3001/questions')
+    .then(res=>res.json())
+    .then((data)=>{
+      setQuestionData(data);
+      setIsLoaded(!isLoaded);
+    })
+    .catch(error=>console.log(error))
+  }, [])
+
+  if(!isLoaded){return <h1>...Loading</h1>}
+
   return (
     <div className="App">
       <h1>Project</h1>

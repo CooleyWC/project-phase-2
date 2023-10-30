e8884943fff3ce3984bc380cf95167a21af2c9d4
Will Cooley <william.cooley@gmail.coom>
2023-10-30 02:52:41 -0400

    added review button

--- my-app/src/QuestionCard.js
+++ my-app/src/QuestionCard.js
@@ -10,12 +10,19 @@ const handleCardClick = ()=>{
     setCardFront(!cardFront)
 }
 
+const handleReviewClick = ()=>{
+    //get id 
+    //need to figure out how the button click doesnt get affected by the card click
+    console.log('i need reviewed')
+}
+
     return (
         <div className='question-card'
             onClick={handleCardClick}
         >
             {cardFront ? <h3>{questionTitle}</h3>
-            : <><p>{questionAnswer}</p><img className='code-image' src={questionImage} alt='uh oh'/> </>}
+            : <><p>{questionAnswer}</p><img className='code-image' src={questionImage} alt='uh oh'/><button onClick={()=>handleReviewClick()}>Add to Review</button> </>}
+            
         </div>
     );
 };

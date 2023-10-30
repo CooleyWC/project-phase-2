639c32061fb9f2750abdffaeaf628031ea51d6f9
Will Cooley <william.cooley@gmail.coom>
2023-10-30 02:47:02 -0400

    added category filter - need to add functionality

--- my-app/src/Filter.js
+++ my-app/src/Filter.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { useOutletContext } from 'react-router-dom';
 
 const Filter = () => {
     const {questionData} = useOutletContext();
-    console.log(questionData)
+    const [dropDownSelect, setdropDownSelect] = useState('select a category')
+   
+
+    const handleSelect = (e)=>{
+        setdropDownSelect(e.target.value);
+        console.log(dropDownSelect)
+    }
+
     return (
         <div>
             <h1>Filter</h1>
+            <select onChange={handleSelect}>
+                <option value='select-a-category'>select a category</option>
+                <option value='jsx'>JSX</option>
+                <option value='componenets-props'>components-props</option>
+                <option value='state'>state</option>
+            </select>
         </div>
     );
 };
